61a718b6efa14a55043c96d3f796f33097fd77a1
krits33 <kritikabissessur03@gmail.com>
2022-07-22 22:40:24 +0400

    [v1.0]2 joueurs

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -1,60 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>    
-/*
-void plus_moins(){
-    int choix_joeuer;
-    printf("Entrer votre choix de chiffre :");
-    scanf("%d", &choix_joeuer);
-}
-*/
+#include<time.h> 
+
 int main(){
-    int choix_joueur;
-    int difficulte;
+    int choix_joeuer;
+    int tries;
     int choix = 0;
     int counter = 0;
     int valeur_a_trouver;
+    int score;
 
     srand(time(NULL));
     valeur_a_trouver = (rand() % 100) + 1;
-    printf("%d\n", valeur_a_trouver); 
-    
-    printf("les differents niveaux sont : \n 1. facile (nombres d'essaie illimite) \n 2. moyen (nombre d'essaie 25) \n 3. difficile (nombre d'essaie 10) \n ");
-    printf("\nVeuillez entrer votre choix de niveau: ");
-    scanf("%d", &choix);
+    printf("%d\n", valeur_a_trouver);
 
-    switch (choix){
-        case 1:
-            do{
-                printf("Entrer votre choix de chiffre :");
-                scanf("%d", &choix_joueur);
-                counter++;
-            }while(choix_joueur != valeur_a_trouver); 
-            printf("bingo\n");           
-            break;
+    printf("JOUEUR 1 - entrer le nombre d'essaie pour JOUEUR 2 :");
+    scanf("%d", &tries);
+
+    do{
+        printf("JOUEUR 2 - entrer votre choix de chiffre :");
+        scanf("%d", &choix_joeuer);
+        counter++;
+    }while((choix_joeuer != valeur_a_trouver) && (counter < tries)); 
+    score = tries - counter;
+    printf("Le score de JOUEUR 1 est - %d \n", score); 
+
+    return 0;          
             
-        case 2:
-            do{
-                printf("Entrer votre choix de chiffre :");
-                scanf("%d", &choix_joueur);
-                counter++;
-            }while((choix_joueur != valeur_a_trouver) && (counter < 25)); 
-            if(choix_joueur == valeur_a_trouver){
-               printf("bingo\n");  
-            }                      
-            break;
-
-        case 3:
-            do{
-                printf("Entrer votre choix de chiffre :");
-                scanf("%d", &choix_joueur);
-                counter++;
-            }while((choix_joueur != valeur_a_trouver) && (counter < 10)); 
-            if(choix_joueur == valeur_a_trouver){
-               printf("bingo\n");  
-            }          
-            break;
-    }    
-
-    return 0;
 }
